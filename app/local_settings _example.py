498b4109b4d105f1237f6ea1a1341f4c573ccdc4
UbuntuTom <gkevin964@gmail.com>
2021-04-01 16:41:27 -0600

    agragado el ejemplo de local settings

---
+++ app/local_settings _example.py
@@ -0,0 +1,11 @@
+# settings.py
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'control_ordenes',
+        'USER' : '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306'
+    }
+}
